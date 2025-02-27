2c971f37c78b6852e9636135a2e2e84d1464bf8b
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2025-02-27 02:10:34 -0800

    Update w3q3.py

--- w3q3.py
+++ w3q3.py
@@ -1,17 +1,10 @@
 import math
-
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def euler_totient(n):
-    count = 0
-    for i in range(1, n):
-        if gcd(n, i) == 1:
-            count += 1
+def euler(n):
+    count=0
+    for i in range(1,n):
+        if math.gcd(i,n)==1:
+            count+=1
     return count
 
-num = int(input("Enter a number: "))
-result = euler_totient(num)
-print(f"Euler Totients of {num} is {result}")
+n=int(input("Enter a number: "))
+print("eulers totient is: ",euler(n))
